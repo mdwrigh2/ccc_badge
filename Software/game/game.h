cbc06f0c048be42dd902a7e56d97447fad2dd326
Carson Holgate <clholgat@ncsu.edu>
2010-09-01 11:19:22 -0400

    starting main loop checking and dealing with state

--- Software/game/game.h
+++ Software/game/game.h
@@ -1,6 +1,21 @@
 #ifndef GAME
 #define GAME
 
+//State variables Eww
+byte firstBoot;
+byte state;
+
+//States
+#define BOOT 0
+#define KEYBOARD 2
+#define MAJOR 4
+#define CHARACTER 6
+#define MENU 8
+#define ENEMIES 10
+#define STATS 12
+#define HELP 14
+#define ABOUT 16
+
 //major and character codes
 byte major;
 byte character;
@@ -8,7 +23,7 @@ char *name;
 char *team;
 
 //things that happen in the first boot
-void firstBoot();
+void firstBootSetup();
 void printSplash();
 char *keyboardDialog(char *prompt); //name
 byte chooseMajor();
